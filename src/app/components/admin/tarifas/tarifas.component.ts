171b7ebc57c0ca09db57b77e306b9b712201c30c
SirYorch <jcuevam3@est.ups.edu.ec>
2025-02-06 10:38:21 -0500

    reparacion de conflictos de merge.

--- src/app/components/admin/tarifas/tarifas.component.ts
+++ src/app/components/admin/tarifas/tarifas.component.ts
@@ -6,6 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { MenuComponent } from "../menu/menu.component";
 import { CommonModule } from '@angular/common';
 import { UserInfoService } from '../../../services/user-info.service';
+import { GoogleAuthService } from '../../../services/google-auth.service';
+import { ReadService } from '../../../services/read.service';
 @Component({
   selector: 'app-tarifas',
   standalone: true,
@@ -22,14 +24,7 @@ export class TarifasComponent {
   motd = "0";
   parqueaderoEstado = "0";
   plazasDisponibles = "0";
-  tarifas1 = "0";
-  tarifas2 = "0";
-  tarifas3 = "0";
-  tarifas4 = "0";
-  tarifas5 = "0";
-  tarifas6 = "0";
-  tarifas7 = "0";
-  tarifas8 = "0";
+  tarifas: {descripcion:string , precio:number}[] = [];
 
   
   guardarDatos(){
@@ -37,15 +32,7 @@ export class TarifasComponent {
       {
         motd:this.motd,
         parqueaderoEstado: this.parqueaderoEstado,
-        plazasDisponibles:this.plazasDisponibles,
-        tarifas1:this.tarifas1,
-        tarifas2:this.tarifas2,
-        tarifas3:this.tarifas3,
-        tarifas4:this.tarifas4,
-        tarifas5:this.tarifas5,
-        tarifas6:this.tarifas6,
-        tarifas7:this.tarifas7,
-        tarifas8:this.tarifas8,
+        plazasDisponibles:this.plazasDisponibles
       }
     );
   }
